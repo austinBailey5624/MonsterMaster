3439ad693f4eaa814dec460d2cf108a7944b150a
Austin Bailey <a604b997@ku.edu>
2016-05-09 15:13:55 -0500

    Create Battlefield class

---
+++ MonsterMaster/src/ver0/Battlefield.java
@@ -0,0 +1,15 @@
+/**
+ * @fileName:		Battlefield.java
+ * @author:			Austin Bailey <austin5624@gmail.com>
+ * @dateBegun:		5/8/2016
+ * @LatestUpdate:	5/8/2016
+ */
+package ver0;
+
+public class Battlefield 
+{
+	Actor[] m_allies;
+	Actor[] m_enemies;
+	int m_curWeather=0;
+	
+}
