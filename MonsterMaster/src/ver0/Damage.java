96e78c33f5d0fb7a062b59e9dd17a1a73cdc7cbf
Austin Bailey <a604b997@ku.edu>
2016-08-31 13:04:36 -0500

    Added constructors and a member variable to stand for statuses in battle

--- MonsterMaster/src/ver0/Damage.java
+++ MonsterMaster/src/ver0/Damage.java
@@ -2,13 +2,47 @@ package ver0;
 
 public class Damage 
 {
-	int m_PhyDmg;
-	int m_MagDmg;
-	int m_FirDmg;
-	int m_WtrDmg;
-	int m_ErtDmg;
-	int m_AirDmg;
-	int m_LgtDmg;
-	int m_DrkDmg;
-	int[] m_StatusesDmg;
+	static int numStatuses=21;
+	int m_phyDmg;
+	int m_magDmg;
+	int m_firDmg;
+	int m_wtrDmg;
+	int m_ertDmg;
+	int m_airDmg;
+	int m_lgtDmg;
+	int m_drkDmg;
+	int[] m_statusesDmg;
+	
+	public Damage()
+	{
+		m_phyDmg=0;
+		m_magDmg=0;
+		m_firDmg=0;
+		m_wtrDmg=0;
+		m_ertDmg=0;
+		m_airDmg=0;
+		m_lgtDmg=0;
+		m_drkDmg=0;
+		m_statusesDmg=new int[numStatuses];
+		for(int i=0;i<m_statusesDmg.length;i++)
+		{
+			m_statusesDmg[i]=0;
+		}
+	}
+	public Damage(int phyDmg)
+	{
+		m_phyDmg=phyDmg;
+		m_magDmg=0;
+		m_firDmg=0;
+		m_wtrDmg=0;
+		m_ertDmg=0;
+		m_airDmg=0;
+		m_lgtDmg=0;
+		m_drkDmg=0;
+		m_statusesDmg=new int[numStatuses];
+		for(int i=0;i<m_statusesDmg.length;i++)
+		{
+			m_statusesDmg[i]=0;
+		}
+	}
 }
