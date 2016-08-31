ef14a997a4c5ed7dd2b8dd47fd6442a63c1732fc
Austin Bailey <a604b997@ku.edu>
2016-08-31 14:07:12 -0500

    Implemented 9 skills

--- MonsterMaster/src/ver0/Skill.java
+++ MonsterMaster/src/ver0/Skill.java
@@ -5,15 +5,15 @@ public class Skill
 	String m_name;
 	int m_element;
 	int m_index;
-	boolean m_isPhysical;
-	boolean m_isMagical;
-	boolean m_castOnSingle;
-	boolean m_castOnEnemy;
-	Damage m_damage;
+	boolean m_isPhysical;		//determines if the physical counter is incremented after execution
+	boolean m_isMagical;		//determines if the magical counter is incremented after execution 
+	boolean m_castOnSelf;		//determines if the skill can only be used by the user on the user
+	boolean m_castOnSingle;		//determines if the skill is cast on a single character or multiple
+	boolean m_castOnEnemy;		//determines if the skill is cast on allies or enemies
+	Damage m_damage;			//The damage quantity that will be inflicted on the target-can be empty
 	public static Skill[] getAllSkills()
 	{
-		MonsterType[] bestiary=Bestiary.convertTo1d();
-		Skill[] skillSet=new Skill[2];
+		Skill[] skillSet=new Skill[9];
 		for(int i=0;i<skillSet.length;i++)
 		{
 			skillSet[i]=new Skill(i);
@@ -22,6 +22,9 @@ public class Skill
 	}
 	public Skill(int index)
 	{
+		m_castOnSelf=false;
+		m_castOnSingle=false;
+		m_castOnEnemy=false;
 		m_index=index;
 		m_damage = new Damage();
 		if(index==0)
@@ -29,6 +32,72 @@ public class Skill
 			m_name="PyroShell";
 			m_isPhysical=true;
 			m_isMagical=true;
+			m_castOnSelf=true;
+		}
+		else if(index==1)
+		{
+			m_name="AquaShell";
+			m_isPhysical=true;
+			m_isMagical=true;
+			m_castOnSelf=true;
+		}
+		else if(index==2)
+		{
+			m_name="TeraShell";
+			m_isPhysical=true;
+			m_isMagical=true;
+			m_castOnSelf=true;
+		}
+		else if(index==3)
+		{
+			m_name="AeroShell";
+			m_isPhysical=true;
+			m_isMagical=true;
+			m_castOnSelf=true;
+		}
+		else if(index==4)
+		{
+			m_name="LuxorShell";
+			m_isPhysical=true;
+			m_isMagical=true;
+			m_castOnSelf=true;
+		}
+		else if(index==5)
+		{
+			m_name="UmbralShell";
+			m_isPhysical=true;
+			m_isMagical=true;
+			m_castOnSelf=true;
+		}
+		else if(index==6)//for flarial [0][0][1][0]
+		{
+			m_name="Fire Slam";
+			m_isPhysical=true;
+			m_isMagical=false;
+			m_castOnSelf=false;
+			m_castOnSingle=true;
+			m_castOnEnemy=true;
+		}
+		else if(index==7)//for Pyrus
+		{
+			m_name="Burn Bite";
+			m_isPhysical=true;
+			m_isMagical=false;
+			m_castOnSelf=false;
+			m_castOnSingle=true;
+			m_castOnEnemy=true;
+		}
+		else if(index==8)//for Pyros
+		{
+			m_name="Red Bull Charge";
+			m_isPhysical=true;
+			m_isMagical=false;
+			m_castOnSelf=false;
+			m_castOnSingle=true;
+			m_castOnEnemy=true;
+		}
+		if(m_castOnSelf==true)
+		{
 			m_castOnSingle=true;
 			m_castOnEnemy=false;
 		}
@@ -41,11 +110,46 @@ public class Skill
 		}
 		if(m_index==0)
 		{
-			target[0].m_thornsDmg[0]=5;
+			target[0].m_thornsDmg[2]=5;
 		}
 		else if(m_index==1)
 		{
-			
+			target[0].m_thornsDmg[3]=5;
+		}
+		else if(m_index==2)
+		{
+			target[0].m_thornsDmg[4]=5;
+		}
+		else if(m_index==3)
+		{
+			target[0].m_thornsDmg[5]=5;
+		}
+		else if(m_index==4)
+		{
+			target[0].m_thornsDmg[6]=5;
+		}
+		else if(m_index==5)
+		{
+			target[0].m_thornsDmg[7]=5;
+		}
+		else if(m_index==6)
+		{
+			target[0].m_thornsDmg[8]=5;
+		}
+		else if(m_index==7)
+		{
+			Damage myDamage=new Damage();
+			myDamage.m_firDmg=10;
+			myDamage.m_phyDmg=10;
+			target[0].takeDamage(myDamage);
+		}
+		else if(m_index==8)
+		{
+			Damage myDamage=new Damage();
+			myDamage.m_firDmg=30;
+			myDamage.m_phyDmg=20;
+			myDamage.m_statusesDmg[0]=2;
+			target[0].takeDamage(myDamage);
 		}
 	}
 	public static String getElementName(int i)
