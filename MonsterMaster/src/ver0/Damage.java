bdfea1467e8db1769fb8d2cd0a19ebad787f3639
Austin Bailey <a604b997@ku.edu>
2016-08-31 11:00:58 -0500

    made damage class for fight calculation

---
+++ MonsterMaster/src/ver0/Damage.java
@@ -0,0 +1,14 @@
+package ver0;
+
+public class Damage 
+{
+	int m_PhyDmg;
+	int m_MagDmg;
+	int m_FirDmg;
+	int m_WtrDmg;
+	int m_ErtDmg;
+	int m_AirDmg;
+	int m_LgtDmg;
+	int m_DrkDmg;
+	int[] m_StatusesDmg;
+}
