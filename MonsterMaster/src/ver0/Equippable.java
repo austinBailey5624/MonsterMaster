badb879a26efe4816dfbe9b0be1d9dfb84394eef
Austin Bailey <a604b997@ku.edu>
2016-09-08 12:17:55 -0500

    Updated Item classes to use arrays for damage

--- MonsterMaster/src/ver0/Equippable.java
+++ MonsterMaster/src/ver0/Equippable.java
@@ -4,25 +4,13 @@ public class Equippable extends Item
 {
 	static public int numEquippable = 4;
 	
-	//member variables
+	//member variables -make private
 	String m_type;
-	int m_phyDmg;
-	int m_phyDef;
-	int m_firDmg;
-	int m_firDef;
-	int m_wtrDmg;
-	int m_wtrDef;
-	int m_ertDmg;
-	int m_ertDef;
-	int m_airDef;
-	int m_airDmg;
-	int m_lgtDef;
-	int m_lgtDmg;
-	int m_drkDef;
-	int m_drkDmg;
+	int[] m_dmgBonus;
+	int[] m_defBonus;
 	
 	//constructors
-	public Equippable()
+	public Equippable()//this empty constructor is here for the benefit of the next one
 	{
 		
 	}
@@ -30,25 +18,18 @@ public class Equippable extends Item
 	public Equippable(int which)
 	{
 		m_description="";
-		m_phyDmg=0;
-		m_phyDef=0;
-		m_firDmg=0;
-		m_firDef=0;
-		m_wtrDmg=0;
-		m_wtrDef=0;
-		m_ertDmg=0;
-		m_ertDef=0;
-		m_airDmg=0;
-		m_airDef=0;
-		m_lgtDmg=0;
-		m_lgtDef=0;
-		m_drkDmg=0;
-		m_drkDef=0;
+		m_dmgBonus = new int[8];
+		m_defBonus = new int[8];
+		for(int i=0; i<m_dmgBonus.length;i++)
+		{
+			m_dmgBonus[i]=0;//initializes all of the values to 0
+			m_defBonus[i]=0;
+		}
 		if(which==0)
 		{
 			m_type="1h weapon";
 			m_name="rusty iron dagger";
-			m_phyDmg=5;
+			m_dmgBonus[0]=5;
 			m_description="This near useless iron dagger is so crusty, you imagine it could break at any moment.\n the edges of the blade are flaking off, and in the crevices\n there is a reddish orange rust.";
 			m_value=3;
 		}
@@ -56,7 +37,7 @@ public class Equippable extends Item
 		{
 			m_type="Sheild";
 			m_name="flat log";
-			m_phyDef=5;
+			m_defBonus[0]=5;
 			m_description="Its not really much of a shield, just a log with a twig that makes it easy to hold.\n It can be easily broken through, and when hit, the log\n slacks against your arm painfully.";
 			m_value=3;
 		}
@@ -64,7 +45,7 @@ public class Equippable extends Item
 		{
 			m_type="Armor";
 			m_name = "sackcloth shirt";
-			m_phyDef=3;
+			m_defBonus[0]=3;
 			m_description="What happens when the sacks that hold grain are no longer needen?\n Theyre turned into shirts apparently.";
 			m_value=3;
 		}
@@ -72,7 +53,7 @@ public class Equippable extends Item
 		{
 			m_type = "Ring";
 			m_name = "Unadorned Iron ring";
-			m_phyDef=1;
+			m_defBonus[0]=1;
 			m_description = "This Iron ring seems to be completely useless,\n certainly not a display of wealth, or station.\n Perhaps it serves to remind the bearer where they came from";
 			m_value=10;
 		}
--- MonsterMaster/src/ver0/Useable.java
+++ MonsterMaster/src/ver0/Useable.java
@@ -8,13 +8,7 @@ public class Useable extends Item
 	//member variables
 	//TODO make member variables private and write setters/getters
 	boolean m_targetsMultiple;
-	int m_phyDmg;
-	int m_firDmg;
-	int m_wtrDmg;
-	int m_ertDmg;
-	int m_airDmg;
-	int m_lgtDmg;
-	int m_drkDmg;
+	int[] m_damage;
 	
 	//constructors
 	public Useable()
@@ -26,66 +20,64 @@ public class Useable extends Item
 		//sets these values to default values unless changed later
 		m_targetsMultiple=false;
 		m_description="";
-		m_phyDmg=0;
-		m_firDmg=0;
-		m_wtrDmg=0;
-		m_ertDmg=0;
-		m_airDmg=0;
-		m_lgtDmg=0;
-		m_drkDmg=0;
+		m_damage = new int[8];
+		for(int i=0;i<m_damage.length;i++)
+		{
+			m_damage[i]=0;
+		}
 		if(which==0)
 		{
 			m_name = "lesser healing potion";
 			m_value=25;
-			m_phyDmg=-50;
+			m_damage[0]=-50;
 		}
 		else if(which==1)
 		{
 			m_name = "spike";
 			m_value=20;
-			m_phyDmg=25;
+			m_damage[0]=25;
 		}
 		else if(which==2)
 		{
 			m_name = "pepperspice";
-			m_firDmg=20;
+			m_damage[3]=20;
 			m_value=20;
 		}
 		else if(which==3)
 		{
 			m_name = "ice water";
-			m_wtrDmg=20;
+			m_damage[4]=20;
 			m_value = 20;
 		}
 		else if(which==4)
 		{
 			m_name = "mystic dust";
-			m_ertDmg = 20;
+			m_damage[5] = 20;
 			m_value = 20;
 		}
 		else if(which==5)
 		{
 			m_name = "captured wind";
-			m_airDmg=20;
+			m_damage[6]=20;
 			m_value=20;
 		}
 		else if(which==6)
 		{
 			m_name = "reflected starlight";
-			m_lgtDmg=20;
+			m_damage[7]=20;
 			m_value=20;
 		}
 		else if(which==7)
 		{
 			m_name = "contained spite";
-			m_drkDmg=20;
+			m_damage[8]=20;
 			m_value=20;
 		}
 		else if(which==8)
 		{
 			m_name = "Lesser firebomb";
-			m_phyDmg=15;
-			m_firDmg = 15;
+			m_damage[0]=15;
+			m_damage[2] = 15;
 			m_value=30;
 		}
 	}
