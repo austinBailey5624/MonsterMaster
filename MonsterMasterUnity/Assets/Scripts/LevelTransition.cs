71e0a20f7d1eae35e4edd84d9112de9c5c381781
Austin Bailey <a604b997@gmail.com>
2021-01-20 19:28:48 -0600

    Make Scene Transition objects

--- MonsterMasterUnity/Assets/Scripts/LevelTransition.cs
+++ MonsterMasterUnity/Assets/Scripts/LevelTransition.cs
@@ -16,10 +16,15 @@ public class LevelTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        //if(transform.position.x == adjacentTransform.position.x && transform.position.y == adjacentTransform.position.y && Input.GetMouseButtonDown(0))
+        if((transform.position - adjacentTransform.position).sqrMagnitude < 0.1f && Input.GetMouseButtonDown(0))
         {
             SceneManager.LoadScene(nextSceneName);
-
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("ourPosition:" + transform.position + "  theirPosition:" + adjacentTransform.position + "  equals" + (transform.position.x.Equals(adjacentTransform.position.x)));
+            
         }
     }
 }
