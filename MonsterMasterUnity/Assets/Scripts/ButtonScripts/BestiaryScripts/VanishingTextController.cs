a7e3131be4cb885022cd6eb8ba6b0625bf536f81
Austin Bailey <a640b997@gmail.com>
2021-04-08 08:38:37 -0500

    Fix Air primary symbols in bestairy

--- MonsterMasterUnity/Assets/Scripts/ButtonScripts/BestiaryScripts/VanishingTextController.cs
+++ MonsterMasterUnity/Assets/Scripts/ButtonScripts/BestiaryScripts/VanishingTextController.cs
@@ -23,7 +23,27 @@ public class VanishingTextController : MonoBehaviour
         {"Terra",14},
         {"Pegasus",15},
         {"Compassion",16},
-        {"Rotten",17} };
+        {"Rotten",17},
+        {"Phoenix",18},
+        {"Seabreeze",19},
+        {"Bird",20},
+        {"Aero",21 },
+        {"Cocka",22 },
+        {"Nightwing",23 },
+        {"Solar",24 },
+        {"Lunar",25 },
+        {"Guardian",26 },
+        {"Angel",27 },
+        {"Luxor",28 },
+        {"Redemption",29 },
+        {"Demon",30 },
+        {"Poison",31 },
+        {"Undead",32 },
+        {"Storm",33 },
+        {"Betrayal",34 },
+        {"Umbral",35 }
+    };
+
 
 
     public void appearText(string subelementName)
