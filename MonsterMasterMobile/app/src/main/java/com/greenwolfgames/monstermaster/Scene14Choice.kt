c89f921becdb2ce34bc0a5c4ce6b5cbc88e6c8f2
Austin Bailey <a604b997@gmail.com>
2024-03-30 19:00:17 -0500

    main Saturday March 30 2024 Fix some bugs, update server project with more skill rough drafts

--- MonsterMasterMobile/app/src/main/java/com/greenwolfgames/monstermaster/Scene14Choice.kt
+++ MonsterMasterMobile/app/src/main/java/com/greenwolfgames/monstermaster/Scene14Choice.kt
@@ -1,22 +1,7 @@
 package com.greenwolfgames.monstermaster
 
-class Scene14Choice(val text: String, val nextNodeIndex: Int, var stateChange: (State) -> Unit = {}, val element: Element) {
+class Scene14Choice(val prompt: String, val nextNodeIndex: Int, var stateChange: (State) -> Unit = {}, val element: Element) {
 
     constructor(text: String, nextNodeIndex: Int) : this(text, nextNodeIndex, {}, Element.NEUTRAL)
-    fun getText(): String {
-        return text
-    }
 
-    //NOTE: index 0 represent ending the conversation
-    fun getNextNodeIndex(): Int {
-        return nextNodeIndex
-    }
-
-    fun getStateChange(): (State) -> Unit {
-        return stateChange
-    }
-
-    fun getElement(): Element {
-        return element
-    }
 }
--- MonsterMasterMobile/app/src/main/java/com/greenwolfgames/monstermaster/Scene14Node.kt
+++ MonsterMasterMobile/app/src/main/java/com/greenwolfgames/monstermaster/Scene14Node.kt
@@ -3,15 +3,4 @@ package com.greenwolfgames.monstermaster
 class Scene14Node(val index: Int, val prompt: String, val choices: List<Scene14Choice>)
 {
 
-    fun getIndex(): Int {
-        return index
-    }
-
-    fun getPrompt(): String {
-        return prompt
-    }
-
-    fun getChoices(): List<Scene14Choice> {
-        return choices
-    }
 }
