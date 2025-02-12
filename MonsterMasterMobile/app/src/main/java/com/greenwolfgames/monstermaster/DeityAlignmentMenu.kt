d1e6c29bfde855b6c87ba151bca3b520ce0ec644
Austin Bailey <a604b997@gmail.com>
2025-02-12 07:10:59 -0600

    main Wednesday February 12 2025 Finish up deityalignment menu

--- MonsterMasterMobile/app/src/main/java/com/greenwolfgames/monstermaster/DeityAlignmentMenu.kt
+++ MonsterMasterMobile/app/src/main/java/com/greenwolfgames/monstermaster/DeityAlignmentMenu.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
@@ -28,6 +29,7 @@ class DeityAlignmentMenu : Fragment()
         {
             val fragment = DeityAlignmentMenu()
             val bundle = Bundle()
+            bundle.putParcelable("state", state)
             fragment.arguments = bundle
             return fragment
         }
@@ -57,28 +59,45 @@ class DeityAlignmentMenu : Fragment()
 
         view.findViewById<ImageView>(R.id.menu_deity_alignment_neutral_background).setColorFilter(
             ContextCompat.getColor(requireContext(),
-                Element.getButtonBackgroundColor(Element.INITIAL)), PorterDuff.Mode.MULTIPLY)
+                Element.getButtonBackgroundColor(Element.PHYSICAL)), PorterDuff.Mode.MULTIPLY)
 
         view.findViewById<ImageView>(R.id.menu_deity_alignment_chaotic_background).setColorFilter(
             ContextCompat.getColor(requireContext(),
                 Element.getButtonBackgroundColor(Element.UMBRAL)), PorterDuff.Mode.MULTIPLY)
 
-        view.findViewById<ImageView>(R.id.menu_deity_alignment_lawful_text).setColorFilter(
-            ContextCompat.getColor(requireContext(),
-                Element.getTextColor(Element.LUXOR)), PorterDuff.Mode.MULTIPLY)
+        view.findViewById<TextView>(R.id.menu_deity_alignment_lawful_text)
+            .setTextColor(ContextCompat.getColor(requireContext(),
+                Element.getTextColor(Element.LUXOR)))
 
-        view.findViewById<ImageView>(R.id.menu_deity_alignment_neutral_text).setColorFilter(
-            ContextCompat.getColor(requireContext(),
-                Element.getTextColor(Element.INITIAL)), PorterDuff.Mode.MULTIPLY)
+        view.findViewById<TextView>(R.id.menu_deity_alignment_neutral_text)
+            .setTextColor(ContextCompat.getColor(requireContext(),
+                Element.getTextColor(Element.INITIAL)))
 
-        view.findViewById<ImageView>(R.id.menu_deity_alignment_chaotic_text).setColorFilter(
-            ContextCompat.getColor(requireContext(),
-                Element.getTextColor(Element.UMBRAL)), PorterDuff.Mode.MULTIPLY)
+        view.findViewById<TextView>(R.id.menu_deity_alignment_chaotic_text)
+            .setTextColor(ContextCompat.getColor(requireContext(),
+                Element.getTextColor(Element.UMBRAL)))
 
-        val buttons: List<Button> = listOf(
-            view.findViewById(R.id.menu_deity_back_button),
-            view.findViewById(R.id.menu_deity_info)
-        )
+        val buttons: List<Button> = listOf(view.findViewById(R.id.menu_deity_back_button),
+            view.findViewById(R.id.menu_deity_info))
+
+        colorForDeity(view.findViewById(R.id.menu_deity_anu_background),view.findViewById(R.id.menu_deity_anu_text),Deity.ANU)
+        colorForDeity(view.findViewById(R.id.menu_deity_velhu_background),view.findViewById(R.id.menu_deity_velhu_text),Deity.VELHU)
+        colorForDeity(view.findViewById(R.id.menu_deity_sid_background),view.findViewById(R.id.menu_deity_sid_text),Deity.SID)
+        colorForDeity(view.findViewById(R.id.menu_deity_gabriel_background),view.findViewById(R.id.menu_deity_gabriel_text),Deity.GABRIEL)
+        colorForDeity(view.findViewById(R.id.menu_deity_ahros_background),view.findViewById(R.id.menu_deity_ahros_text),Deity.AHROS)
+        colorForDeity(view.findViewById(R.id.menu_deity_pythion_background),view.findViewById(R.id.menu_deity_pythion_text),Deity.PYTHION)
+        colorForDeity(view.findViewById(R.id.menu_deity_bennu_background),view.findViewById(R.id.menu_deity_bennu_text),Deity.BENNU)
+        colorForDeity(view.findViewById(R.id.menu_deity_neptune_background),view.findViewById(R.id.menu_deity_neptune_text),Deity.NEPTUNE)
+        colorForDeity(view.findViewById(R.id.menu_deity_gaia_background),view.findViewById(R.id.menu_deity_gaia_text),Deity.GAIA)
+        colorForDeity(view.findViewById(R.id.menu_deity_bacchus_background),view.findViewById(R.id.menu_deity_bacchus_text),Deity.BACCHUS)
+        colorForDeity(view.findViewById(R.id.menu_deity_mars_background),view.findViewById(R.id.menu_deity_mars_text),Deity.MARS)
+        colorForDeity(view.findViewById(R.id.menu_deity_thanatos_background),view.findViewById(R.id.menu_deity_thanatos_text),Deity.THANATOS)
+        colorForDeity(view.findViewById(R.id.menu_deity_acheron_background),view.findViewById(R.id.menu_deity_acheron_text),Deity.ACHERON)
+        colorForDeity(view.findViewById(R.id.menu_deity_illian_background),view.findViewById(R.id.menu_deity_illian_text),Deity.ILLIAN)
+        colorForDeity(view.findViewById(R.id.menu_deity_webspinner_background),view.findViewById(R.id.menu_deity_webspinner_text),Deity.WEBSPINNER)
+        colorForDeity(view.findViewById(R.id.menu_deity_muir_background),view.findViewById(R.id.menu_deity_muir_text),Deity.MUIR)
+        colorForDeity(view.findViewById(R.id.menu_deity_malacathe_background),view.findViewById(R.id.menu_deity_malacathe_text),Deity.MALACATHE)
+        colorForDeity(view.findViewById(R.id.menu_deity_enemy_background),view.findViewById(R.id.menu_deity_enemy_text),Deity.ENEMY)
 
         for(button in buttons)
         {
@@ -88,5 +107,12 @@ class DeityAlignmentMenu : Fragment()
         view.findViewById<Button>(R.id.menu_deity_back_button).setOnClickListener{
             requireActivity().supportFragmentManager.popBackStack()
         }
+    }
+
+    private fun colorForDeity(imageView: ImageView, textView: TextView, deity: Deity)
+    {
+        val element: Element = Deity.getElement(deity)
+        imageView.setColorFilter(ContextCompat.getColor(requireContext(), Element.getBackgroundColor(element)), PorterDuff.Mode.MULTIPLY)
+        textView.setTextColor(ContextCompat.getColor(requireContext(), Element.getTextColor(element)))
     }
 }
